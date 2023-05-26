da61d53a5d4c14e3a421aaf313336873711824b2
Manuel <manuellozanobo@gmail.com>
2023-05-25 23:24:15 -0500

    new handlebar change

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app=express();
 const port=8080;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+const viewsRouter= require( './routes/views-router.js');
+const { __dirname, __filename } = require('./utils.js') ;
+const handlebars = require('express-handlebars') ;
+const http = require('http') ;
+const { Server as SocketServer }= require('socket.io') ;//Modificar esta parte del server as socketServer 
+const io = new SocketServer(httpServer);
 
 const productRoute=require('./routes/products.route');
 app.use('/api/products', productRoute);
